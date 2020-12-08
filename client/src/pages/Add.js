a237aea70503cf63e64825b7335a79c25721d476
Michael Zhao <michaelzhao314@gmail.com>
2020-12-08 01:50:06 -0600

    Updated add event placeholder text

--- client/src/pages/Add.js
+++ client/src/pages/Add.js
@@ -5,7 +5,7 @@ class Add extends React.Component {
     render() {
         return(
             <div className="Add">
-                Add!
+                Add Event!
             </div>
         )
     }
