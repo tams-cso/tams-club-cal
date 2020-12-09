44bdfbaeea9ba61b40943c0ee9e87aa8affc9c93
Michael Zhao <michaelzhao314@gmail.com>
2020-12-09 17:38:35 -0600

    Added api.js framework

---
+++ client/src/functions/api.js
@@ -0,0 +1,11 @@
+getEvents = () => {
+    fetch('https://api.tams.club/events')
+        .then((res) => res.json())
+        .then((json) => console.log(json));
+};
+
+// TODO: Add the rest of the requests
+// see https://github.com/MichaelZhao21/playlists-plus/blob/master/src/components/spotify-api.js
+// for example fetch POST requests
+
+export { getEvents };
