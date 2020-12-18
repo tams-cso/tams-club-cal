fc04f9d1e03aae8e36890945b91f4a5d0f411e0b
Michael Zhao <michaelzhao314@gmail.com>
2020-12-18 16:35:36 -0600

    Added temp backend stuff

--- client/src/functions/api.js
+++ client/src/functions/api.js
@@ -1,11 +1,15 @@
-getEvents = () => {
+function getEvents() {
     fetch('https://api.tams.club/events')
         .then((res) => res.json())
         .then((json) => console.log(json));
-};
+}
+
+async function getClubList() {
+    return await fetch('http://localhost:5000/clubs').then((res) => res.json());
+}
 
 // TODO: Add the rest of the requests
 // see https://github.com/MichaelZhao21/playlists-plus/blob/master/src/components/spotify-api.js
 // for example fetch POST requests
 
-export default { getEvents };
+export { getEvents, getClubList };
--- client/src/pages/Clubs.js
+++ client/src/pages/Clubs.js
@@ -1,56 +1,17 @@
 import React from 'react';
 import ClubCard from '../components/ClubCard';
+import { getClubList } from '../functions/api';
 import './Clubs.scss';
 
 class Clubs extends React.Component {
     constructor(props) {
         super(props);
-
-        // TODO: Replace with fetched data from backend/cache
-        this.clubList = [
-            {
-                name: 'CSO (Computer Science Organization)',
-                advised: 'true',
-                fb: 'https://www.facebook.com/groups/cso2021',
-                website: 'https://cso.tams.club',
-                coverImg: 'https://api.michaelzhao.xyz/static/club-cal/cso.png',
-            },
-            {
-                name: 'HOPE (Helping Other People Everywhere)',
-                advised: 'true',
-                fb: 'https://www.facebook.com/groups/hope2021',
-                website: 'https://tamshope.org',
-                coverImg: 'https://api.michaelzhao.xyz/static/club-cal/hope.png',                
-            },
-            {
-                name: 'Ambassadors',
-                advised: 'true',
-                fb: 'https://www.facebook.com/groups/435813770636084',
-                website: '',
-                coverImg: 'https://api.michaelzhao.xyz/static/club-cal/ambassadors.png',                
-            },
-            {
-                name: 'TCS (TAMS Culinary Society)',
-                advised: 'false',
-                fb: 'https://www.facebook.com/groups/481790829408149',
-                website: '',
-                coverImg: 'https://api.michaelzhao.xyz/static/club-cal/tcs.png',                
-            },
-            {
-                name: 'NACRA (Nihon Arts and Culture Research)',
-                advised: 'false',
-                fb: 'https://www.facebook.com/groups/416640248899431',
-                website: '',
-                coverImg: 'https://api.michaelzhao.xyz/static/club-cal/nacra.png',                
-            },
-        ];
-
         this.state = {};
     }
 
-    createCards = () => {
+    createCards = (data) => {
         var clubCards = [];
-        this.clubList.forEach((club) => {
+        data.forEach((club) => {
             clubCards.push(
                 <ClubCard
                     name={club.name}
@@ -58,6 +19,7 @@ class Clubs extends React.Component {
                     fb={club.fb}
                     website={club.website}
                     coverImg={club.coverImg}
+                    key={club.name}
                 ></ClubCard>
             );
         });
@@ -65,15 +27,15 @@ class Clubs extends React.Component {
     };
 
     componentDidMount() {
-        this.createCards();
+        getClubList().then((data) => {
+            this.createCards(data);
+        });
     }
 
     render() {
         return (
             <div className="Clubs">
-                <div className="club-card-list">
-                    {this.state.clubCards}
-                </div>
+                <div className="club-card-list">{this.state.clubCards}</div>
             </div>
         );
     }
