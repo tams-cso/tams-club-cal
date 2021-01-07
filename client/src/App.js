fc543b2e24ff4b79c5da9e7499bc32d60ad48979
royantar0311 <royantar1@gmail.com>
2021-01-07 15:07:49 +0600

    page-not-found-updated-#84

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.scss';
 import Menu from './components/Menu';
 import About from './pages/About';
@@ -15,12 +15,14 @@ function App() {
                 <Menu />
                 <div className="menu-bkgd"></div>
                 <div className="page">
-                    <Route exact path={["/", "/event"]} component={Home} />
-                    <Route exact path="/resources" component={Resources} />
-                    <Route exact path="/clubs" component={Clubs} />
-                    <Route exact path="/add" component={Add} />
-                    <Route exact path="/about" component={About} />
-                    <Route component={NotFound}/>
+                    <Switch>
+                        <Route exact path={["/", "/event"]} component={Home} />
+                        <Route exact path="/resources" component={Resources} />
+                        <Route exact path="/clubs" component={Clubs} />
+                        <Route exact path="/add" component={Add} />
+                        <Route exact path="/about" component={About} />
+                        <Route component={NotFound}/>
+                    </Switch>
                 </div>
             </BrowserRouter>
         </div>
