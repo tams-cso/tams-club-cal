0cf069ebbd6b1b146b6127073ae03dcc4771cae8
MichaelZhao21 <MichaelZhao21@users.noreply.github.com>
2021-01-08 10:03:47 +0000

    Apply formatting changes to edit-volunteering

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,16 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const { getClubList, getClub, addFeedback, addEvent, getEvent, getEventList, getVolunteering, updateVolunteering } = require('./database');
+const {
+    getClubList,
+    getClub,
+    addFeedback,
+    addEvent,
+    getEvent,
+    getEventList,
+    getVolunteering,
+    updateVolunteering,
+} = require('./database');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -62,6 +71,6 @@ app.post('/add-event', async (req, res, next) => {
 app.post('/add-volunteering', async (req, res, next) => {
     if (req.query.update === 'true') await updateVolunteering(req.body, req.query.id);
     res.sendStatus(200);
-})
+});
 
 app.listen(process.env.PORT | 5000, () => console.log(`Listening on port ${process.env.PORT | 5000}`));
