f9e71448bf5eca056541ed39998e0886f4bcf76c
Michael <michaelzhao314@gmail.com>
2021-01-08 11:10:42 -0600

    Formatted 404 page

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -9,15 +9,14 @@ class NotFound extends React.Component {
     
     render() {
         return <div className="error-message">
-                <h3>The page you are trying to access could not be found 😔</h3>
+                <h1>The page you are trying to access could not be found 😔</h1>
 
-                    But here are some cool pages that you could go to 😃
-                    <br/>
+                    <h2>But here are some cool pages that you could go to 😃</h2>
                     <ul  >
-                        <li><Link className="page-not-found-nav-item" to='/'>Homepage <span className="helper-text">(has the events)  ➡️</span></Link></li>
-                        <li><Link className="page-not-found-nav-item" to='/resources'>Resources <span className="helper-text">(with links and volunteering opportunities)  ➡️</span></Link></li>
-                        <li><Link className="page-not-found-nav-item" to='/clubs'>Clubs <span className="helper-text">(all the clubs at TAMS)  ➡️</span></Link></li>
-                        <li><Link className="page-not-found-nav-item" to='/about'>About <span className="helper-text">(what is this site about and feedback form here)  ➡️</span></Link></li>
+                        <li><Link className="page-not-found-nav-item" to='/'>Homepage <span className="helper-text">(has the events) ➤</span></Link></li>
+                        <li><Link className="page-not-found-nav-item" to='/resources'>Resources <span className="helper-text">(with links and volunteering opportunities) ➤</span></Link></li>
+                        <li><Link className="page-not-found-nav-item" to='/clubs'>Clubs <span className="helper-text">(all the clubs at TAMS) ➤</span></Link></li>
+                        <li><Link className="page-not-found-nav-item" to='/about'>About <span className="helper-text">(what is this site about and feedback form here) ➤</span></Link></li>
                         
                     </ul>
             </div>;
