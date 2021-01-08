9c49259e8ae31ed5141b7ebc466b2fd1e18b452f
Michael Zhao <michaelzhao314@gmail.com>
2021-01-08 13:11:13 -0600

    Added volunteering action

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,4 +1,4 @@
-import { RESET_STATE, SET_EVENT_LIST } from './actionTypes';
+import { RESET_STATE, SET_EVENT_LIST, SET_VOLUNTEERING_LIST } from './actionTypes';
 
 export const resetState = () => ({
     type: RESET_STATE,
@@ -7,4 +7,9 @@ export const resetState = () => ({
 export const setEventList = (eventList) => ({
     type: SET_EVENT_LIST,
     payload: { eventList },
+});
+
+export const setVolunteeringList = (volunteeringList) => ({
+    type: SET_VOLUNTEERING_LIST,
+    payload: { volunteeringList },
 });
