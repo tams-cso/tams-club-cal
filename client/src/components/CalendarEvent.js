1d9a39f4cb0977d2bbd631acba3976e73646d4b8
The-Box-Lord <56173725+The-Box-Lord@users.noreply.github.com>
2021-01-08 20:40:38 -0600

    fix class issue in calendar

--- client/src/components/CalendarEvent.js
+++ client/src/components/CalendarEvent.js
@@ -5,10 +5,10 @@ class CalendarEvent extends React.Component {
     render() {
         return (
             <div className="CalendarEvent">
-                <div className={'event-type event-type-' + this.props.type}></div>
-                <div className="event-time">{this.props.time}</div>
-                <div className="event-club-name">{this.props.club}</div>
-                <div className="event-name">{this.props.name}</div>
+                <div className={'calendar-event-type event-type-' + this.props.type}></div>
+                <div className="calendar-event-time">{this.props.time}</div>
+                <div className="calendar-event-club-name">{this.props.club}</div>
+                <div className="calendar-event-name">{this.props.name}</div>
             </div>
         );
     }
