315f70fc0f1d54306ee71f87c247cdac11dac205
Michael Zhao <michaelzhao314@gmail.com>
2021-01-18 14:42:46 -0600

    Formatted exec card scss

--- client/src/components/ExecCard.js
+++ client/src/components/ExecCard.js
@@ -5,14 +5,14 @@ import './ExecCard.scss';
 class ExecCard extends React.Component {
     render() {
         return (
-            <div className="ExecCard">
+            <div className="exec-card">
                 <img className="exec-img" src={imgUrl(this.props.exec.img)} alt="profile pic"></img>
-                <div className="info">
-                    <div className="name-position-group">
-                        <div className="name">{this.props.exec.name}</div>
-                        <div className="position">{this.props.exec.position}</div>
+                <div className="exec-card-info">
+                    <div className="exec-card-name-position-group">
+                        <div className="exec-card-name">{this.props.exec.name}</div>
+                        <div className="exec-card-position">{this.props.exec.position}</div>
                     </div>
-                    <div className="description">{this.props.exec.description}</div>
+                    <div className="exec-card-description">{this.props.exec.description}</div>
                 </div>
             </div>
         );
