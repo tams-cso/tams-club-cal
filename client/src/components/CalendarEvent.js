4d24dc217c145f1c1355f1fbfa09cda027e96ac1
The-Box-Lord <56173725+The-Box-Lord@users.noreply.github.com>
2021-01-19 23:28:39 -0600

    Fix css formatting

--- client/src/components/CalendarEvent.js
+++ client/src/components/CalendarEvent.js
@@ -4,7 +4,7 @@ import './CalendarEvent.scss';
 class CalendarEvent extends React.Component {
     render() {
         return (
-            <div className="CalendarEvent" onClick={this.props.onClick}>
+            <div className="calendar-event" onClick={this.props.onClick}>
                 <div className={'calendar-event-type event-type-' + this.props.type}></div>
                 <div className="calendar-event-time">{this.props.time}</div>
                 <div className="calendar-event-name">{this.props.name}</div>
