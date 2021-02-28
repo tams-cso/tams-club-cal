0ed3dade600e6a7e4db665703075687a11167984
Michael Zhao <michaelzhao314@gmail.com>
2021-02-27 23:13:18 -0600

    Styled add button

--- client/src/components/shared/add-button.js
+++ client/src/components/shared/add-button.js
@@ -2,14 +2,30 @@ import React from 'react';
 import './add-button.scss';
 
 class AddButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { text: '+' };
+    }
+
     openAdd = () => {
         window.location.href = `${window.location.origin}/edit/${this.props.type}`;
     };
 
+    hoverEvent = (enter) => {
+        var text = '+';
+        if (enter) text = `+ Add ${this.props.type}`;
+        this.setState({ text });
+    };
+
     render() {
         return (
-            <button className="add-button" onClick={this.openAdd}>
-                +
+            <button
+                className={`add-button ${this.props.className} ${this.props.type}`}
+                onClick={this.openAdd}
+                onMouseEnter={this.hoverEvent.bind(this, true)}
+                onMouseLeave={this.hoverEvent.bind(this, false)}
+            >
+                <div className="add-button-text">{this.state.text}</div>
             </button>
         );
     }
