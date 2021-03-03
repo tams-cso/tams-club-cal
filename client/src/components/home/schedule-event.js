0b2bd596ef7a46dad7b41bcfd8b7ff16ba110a88
Michael Zhao <michaelzhao314@gmail.com>
2021-03-03 11:29:21 -0600

    Added tooltips on schedule event

--- client/src/components/home/schedule-event.js
+++ client/src/components/home/schedule-event.js
@@ -6,7 +6,11 @@ class ScheduleEvent extends React.Component {
     render() {
         return (
             <div className="ScheduleEvent" onClick={this.props.onClick}>
-                <p className={'event-type event-type-' + this.props.event.type}></p>
+                <p className={`schedule-event-type ${this.props.event.type}`}>
+                    <span className={`schedule-event-type-tooltip ${this.props.event.type}`}>
+                        {this.props.event.type}
+                    </span>
+                </p>
                 <p className="event-time">{getFormattedTime(this.props.event)}</p>
                 <p className="event-club-name">{this.props.event.club}</p>
                 <p className="event-name">{this.props.event.name}</p>
