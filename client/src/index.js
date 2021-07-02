1070d54ea389bf450537fb8da9df938b41a89ffa
Michael Zhao <michaelzhao314@gmail.com>
2021-07-01 23:17:54 -0500

    Remove node_env logging message and organize imports

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+
 import makeServer from './server';
+import App from './app';
 
 if (process.env.NODE_ENV !== 'production') {
     makeServer();
 }
-
-console.log(process.env.NODE_ENV);
 
 ReactDOM.render(
     <React.StrictMode>
