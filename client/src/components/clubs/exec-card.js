4781ab4f038604afff239534ce70add2d881728f
Michael Zhao <michaelzhao314@gmail.com>
2021-07-19 15:15:08 -0500

    Fixed exec card view for mobile

--- client/src/components/clubs/exec-card.js
+++ client/src/components/clubs/exec-card.js
@@ -4,7 +4,7 @@ import { darkSwitchGrey } from '../../functions/util';
 import { Exec } from '../../functions/entries';
 
 import Box from '@material-ui/core/Box';
-import Paper from '@material-ui/core/Paper';
+import Hidden from '@material-ui/core/Hidden';
 import Typography from '@material-ui/core/Typography';
 import Image from '../shared/image';
 import Paragraph from '../shared/paragraph';
@@ -26,10 +26,24 @@ const useStyles = makeStyles((theme) => ({
     name: {
         fontSize: '1.75rem',
         lineHeight: 1.1,
+        [theme.breakpoints.down('sm')]: {
+            fontSize: '1.1rem',
+        },
+    },
+    position: {
+        [theme.breakpoints.down('sm')]: {
+            fontSize: '0.85rem',
+        },
     },
     description: {
         color: darkSwitchGrey(theme),
+        [theme.breakpoints.down('sm')]: {
+            fontSize: '0.75rem',
+        },
     },
+    mobileDescription: {
+        padding: '0 12px 12px',
+    }
 }));
 
 /**
@@ -42,24 +56,35 @@ const useStyles = makeStyles((theme) => ({
 const ExecCard = (props) => {
     const classes = useStyles();
     return (
-        <Box className={classes.root}>
-            <Image
-                className={classes.image}
-                src={props.exec.img}
-                alt="profile pic"
-                default="/default-profile.webp"
-                raised
-            ></Image>
-            <Box className="exec-card-info">
-                <Typography variant="h2" className={classes.name}>
-                    {props.exec.name}
-                </Typography>
-                <Typography variant="subtitle1">
-                    {props.exec.position}
-                </Typography>
-                <Paragraph text={props.exec.description} className={classes.description} />
+        <React.Fragment>
+            <Box className={classes.root}>
+                <Image
+                    className={classes.image}
+                    src={props.exec.img}
+                    alt="profile pic"
+                    default="/default-profile.webp"
+                    raised
+                ></Image>
+                <Box className="exec-card-info">
+                    <Typography variant="h2" className={classes.name}>
+                        {props.exec.name}
+                    </Typography>
+                    <Typography variant="subtitle1" className={classes.position}>
+                        {props.exec.position}
+                    </Typography>
+                    <Hidden smDown>
+                        <Paragraph text={props.exec.description} className={classes.description} />
+                    </Hidden>
+                </Box>
             </Box>
-        </Box>
+            <Hidden mdUp>
+                <Paragraph
+                    text={props.exec.description}
+                    className={`${classes.description} ${classes.mobileDescription}`}
+                    fontSize="0.75rem"
+                />
+            </Hidden>
+        </React.Fragment>
     );
 };
 
