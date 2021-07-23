e0ae466ad9ba83db79e71c7a562a0d26b0f98ede
Michael Zhao <37679458+MichaelZhao21@users.noreply.github.com>
2021-07-23 12:47:12 -0500

    Fixed staging and production deployment scripts (#338)
    * Check npm version
    
    * Fix node version
    
    * help maybe creating ext folder will work
    
    * Fix typo
    
    * Added nohoist to fix server
    
    * Google auth library the issue?
    
    * Rewrite build and deploy with Docker!
    
    * Fix yaml config syntax
    
    * Fix version for staging to latest
    
    * Updated docker prefix
    
    * Run in detached
    
    * Remove previous docker instance and add name to container
    
    * Use alpine docker base image and fix express port

--- client/src/components/home/event-entry.js
+++ client/src/components/home/event-entry.js
@@ -14,6 +14,7 @@ const useStyles = makeStyles((theme) => ({
         padding: 0,
     },
     wrapper: {
+        width: '100%',
         padding: 16,
         display: 'flex',
         flexDirection: 'row',
