2571cca452d6a2e017b8ad20892213d8e88c7633
Michael Zhao <michaelzhao314@gmail.com>
2021-12-16 17:52:05 -0600

    Added light theme background color

--- client/src/components/shared/page-wrapper.js
+++ client/src/components/shared/page-wrapper.js
@@ -1,16 +1,8 @@
 import React, { useEffect } from 'react';
-import makeStyles from '@mui/styles/makeStyles';
 import { useLocation } from 'react-router';
-import { Helmet } from 'react-helmet';
 
-const useStyles = makeStyles({
-    root: {
-        paddingTop: 16,
-        paddingBottom: (props) => (props.noBottom ? 0 : 16),
-        display: 'flex',
-        height: 'calc(100vh - 64px)',
-    },
-});
+import { Helmet } from 'react-helmet';
+import Box from '@mui/material/Box';
 
 /**
  * Wraps all pages with a simple styling and scroll to top
@@ -18,24 +10,33 @@ const useStyles = makeStyles({
  * @param {object} props React props object
  * @param {boolean} [props.noBottom] If true will not have a bottom padding
  * @param {string} props.title Will change the title to the given title
+ * @param {object} [props.sx] Format the main box
  */
 const PageWrapper = (props) => {
-    const classes = useStyles({ noBottom: props.noBottom });
     const location = useLocation();
 
+    // Scroll to top on route change
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [location]);
 
     return (
-        <div className={`${classes.root} ${props.className || ''}`}>
+        <Box
+            sx={{
+                paddingTop: 2,
+                paddingBottom: (props) => (props.noBottom ? 0 : 2),
+                display: 'flex',
+                height: 'calc(100vh - 64px)',
+                ...props.sx,
+            }}
+        >
             {props.title ? (
                 <Helmet>
                     <title>{`${props.title} - TAMS Club Calendar`}</title>
                 </Helmet>
             ) : null}
             {props.children}
-        </div>
+        </Box>
     );
 };
 
