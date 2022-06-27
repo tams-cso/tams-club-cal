2f8bafeaee5ed574a9ed4130351765c3f4723d15
Michael Zhao <37679458+MichaelZhao21@users.noreply.github.com>
2022-06-27 11:28:33 -0500

    Addded new colors for light theme (#513)

--- client/src/components/volunteering/filter-list.tsx
+++ client/src/components/volunteering/filter-list.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTheme } from '@mui/material';
 import type { Filters } from '../../types';
 
 import List from '@mui/material/List';
@@ -22,29 +23,35 @@ interface FilterListProps {
  * The specific filter will only be shown if it is true.
  */
 const FilterList = (props: FilterListProps) => {
+    const theme = useTheme();
+    const colors =
+        theme.palette.mode === 'light'
+            ? ['#ff9f32', '#ff6677', '#8496ff', '#c466ff']
+            : ['#ffb258', '#ff8b99', '#abb8ff', '#d38cff'];
+
     return (
         <List dense sx={props.sx}>
             <FilterItem
-                icon={<DashboardRoundedIcon htmlColor="#ffb258" />}
-                color="#ffb258"
+                icon={<DashboardRoundedIcon htmlColor={colors[0]} />}
+                color={colors[0]}
                 status={props.filters.limited}
                 name="Limited Spots"
             />
             <FilterItem
-                icon={<EventNoteRoundedIcon htmlColor="#ff8b99" />}
-                color="#ff8b99"
+                icon={<EventNoteRoundedIcon htmlColor={colors[1]} />}
+                color={colors[1]}
                 status={props.filters.semester}
                 name="Semester Long Committment"
             />
             <FilterItem
-                icon={<ScheduleRoundedIcon htmlColor="#abb8ff" />}
-                color="#abb8ff"
+                icon={<ScheduleRoundedIcon htmlColor={colors[2]} />}
+                color={colors[2]}
                 status={props.filters.setTimes}
                 name="Set Time Slots"
             />
             <FilterItem
-                icon={<EventRoundedIcon htmlColor="#d38cff" />}
-                color="#d38cff"
+                icon={<EventRoundedIcon htmlColor={colors[3]} />}
+                color={colors[3]}
                 status={props.filters.weekly}
                 name="Repeats Weekly"
             />
